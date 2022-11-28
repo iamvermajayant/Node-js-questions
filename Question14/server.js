27c058ecc874aa0b23ad5cf19969c1455213c066
Jayant Verma <iamvermajayant@gmail.com>
2022-11-28 15:18:09 +0530

    question 14 done

---
+++ Question14/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req, res){
+    res.send('I am the get request');
+})
+
+
+//This route is used to make any HTTP request whether get put delete 
+app.all('/users', function(req, res, next){
+    console.log('i am the app.all request');
+    next();
+})
+
+app.listen(4000);
