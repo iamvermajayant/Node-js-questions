04e39beb33d665fe58098017a11b24e85379565c
Jayant Verma <iamvermajayant@gmail.com>
2022-11-28 15:50:54 +0530

    new changes

--- Question14/server.js
+++ Question14/server.js
@@ -6,6 +6,9 @@ app.get('/', function(req, res){
     res.send('I am the get request');
 })
 
+app.get('/manthan',(req,res) =>{
+    res.send('I am from page manthan')
+})
 
 //This route is used to make any HTTP request whether get put delete 
 app.all('/users', function(req, res, next){
